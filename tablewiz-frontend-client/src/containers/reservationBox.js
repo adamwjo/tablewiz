3d69d660103fef0d7aa869c514e478cd43e2e444
github username <github email address>
2018-12-14 10:57:32 -0500

    built static components for reservation model

---
+++ tablewiz-frontend-client/src/containers/reservationBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ReservationBox extends React.Component{
+    render(){
+        return(
+            <div>
+                Reservation Box Component
+            </div>
+        )
+    }
+}
